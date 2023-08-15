f2e786d3aab7be2274db18192c8728df8beb69a2
planifyit.com <contact@planifyit.com>
2023-08-15 15:10:43 +0200

    Update builder-panel.js

--- builder-panel.js
+++ builder-panel.js
@@ -1,36 +1,28 @@
 (function() {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-
-    	<form id="form">
-    <fieldset>
-        <legend>Properties</legend>
-        <table>
-           <tr>
-        <td>Model ID</td>
-        <td><input id="builder_model_id" type="text"></td>
-    </tr>
-            
-        </table>
-        <input type="submit" style="display:none;">
-    </fieldset>
-</form>
-
-
         <style>
-     
-            .panel-content {
-                padding: 10px;
+            :host {
+                display: block;
+                padding: 1em;
             }
             label {
                 display: block;
                 margin-bottom: 5px;
             }
         </style>
-        <div class="panel-content">
-            <label for="modelInput">Model:</label>
-            <input type="text" id="modelInput" />
-        </div>
+        <form id="form">
+            <fieldset>
+                <legend>Properties</legend>
+                <table>
+                    <tr>
+                        <td><label for="builder_model_id">Model ID:</label></td>
+                        <td><input id="builder_model_id" type="text"></td>
+                    </tr>
+                </table>
+                <input type="submit" style="display:none;">
+            </fieldset>
+        </form>
     `;
 
     class BuilderPanel extends HTMLElement {
@@ -39,37 +31,34 @@
             this._shadowRoot = this.attachShadow({ mode: 'open' });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
-            this._modelInput = this._shadowRoot.getElementById('modelInput');
+            this._modelInput = this._shadowRoot.getElementById('builder_model_id');
             this._modelInput.addEventListener('input', this._onModelInputChange.bind(this));
         }
 
-_submit(e) {
-    e.preventDefault();
-    this.dispatchEvent(new CustomEvent("propertiesChanged", {
-        detail: {
-            properties: {
-                decimalPlaces: this.decimalPlaces,
-                dimension: this.dimension,
-                measure: this.measure,
-                modelId: this.modelId
-            }
+        _submit(e) {
+            e.preventDefault();
+            this.dispatchEvent(new CustomEvent("propertiesChanged", {
+                detail: {
+                    properties: {
+                        modelId: this.modelId
+                    }
+                }
+            }));
         }
-    }));
-}
-        
+
         _onModelInputChange(event) {
             // Dispatch a custom event or handle the model input change
             let modelChangeEvent = new CustomEvent('modelChange', { detail: { model: this._modelInput.value } });
             this.dispatchEvent(modelChangeEvent);
         }
 
-    set modelId(newModelId) {
-    this._shadowRoot.getElementById("builder_model_id").value = newModelId;
-}
+        set modelId(newModelId) {
+            this._modelInput.value = newModelId;
+        }
 
-get modelId() {
-    return this._shadowRoot.getElementById("builder_model_id").value;
-}
+        get modelId() {
+            return this._modelInput.value;
+        }
     }
 
     customElements.define('sac-cockpit-builder', BuilderPanel);
