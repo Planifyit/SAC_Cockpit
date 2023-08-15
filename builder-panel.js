fa909c3409d391ae3ef4cafc7b1c676155fbe055
planifyit.com <contact@planifyit.com>
2023-08-15 16:08:30 +0200

    Update builder-panel.js

--- builder-panel.js
+++ builder-panel.js
@@ -10,16 +10,20 @@
                 display: block;
                 margin-bottom: 5px;
             }
+            .input-container {
+                margin-bottom: 10px;
+            }
         </style>
         <form id="form">
             <fieldset>
                 <legend>Properties</legend>
-                <table>
-                    <tr>
-                        <td><label for="builder_model_id">Model ID:</label></td>
-                        <td><input id="builder_model_id" type="text"></td>
-                    </tr>
-                </table>
+                <div id="inputList">
+                    <div class="input-container">
+                        <label for="builder_model_id_0">Model ID:</label>
+                        <input id="builder_model_id_0" type="text">
+                    </div>
+                </div>
+                <button id="addLine">+</button>
                 <input type="submit" style="display:none;">
             </fieldset>
         </form>
@@ -30,27 +34,37 @@
             super();
             this._shadowRoot = this.attachShadow({ mode: 'open' });
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-    this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-  
+            this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
+            this._shadowRoot.getElementById("addLine").addEventListener("click", this._addLine.bind(this));
+            this._inputCount = 1; // to keep track of the number of inputs
         }
 
         _submit(e) {
             e.preventDefault();
+            // Handle the submission of multiple model IDs
+            let modelIds = [];
+            for (let i = 0; i < this._inputCount; i++) {
+                modelIds.push(this._shadowRoot.getElementById(`builder_model_id_${i}`).value);
+            }
             this.dispatchEvent(new CustomEvent("propertiesChanged", {
                 detail: {
                     properties: {
-                        modelId: this.modelId
+                        modelIds: modelIds
                     }
                 }
             }));
         }
 
-        set modelId(newModelId) {
-                	this._shadowRoot.getElementById("builder_model_id").value = newModelId;
-        }
-
-        get modelId() {
-            return this._shadowRoot.getElementById("builder_model_id").value;
+        _addLine(e) {
+            e.preventDefault();
+            let newInputContainer = document.createElement('div');
+            newInputContainer.classList.add('input-container');
+            newInputContainer.innerHTML = `
+                <label for="builder_model_id_${this._inputCount}">Model ID:</label>
+                <input id="builder_model_id_${this._inputCount}" type="text">
+            `;
+            this._shadowRoot.getElementById("inputList").appendChild(newInputContainer);
+            this._inputCount++;
         }
     }
 
