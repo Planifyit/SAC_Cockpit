ad926ea21ad98b6eaf1ed1d107c796433e23c0ce
planifyit.com <contact@planifyit.com>
2023-08-15 13:37:22 +0200

    Update builder-panel.js

--- builder-panel.js
+++ builder-panel.js
@@ -1,91 +1,46 @@
 (function() {
-	let template = document.createElement("template");
-	template.innerHTML = `
-	<form id="form">
-    <fieldset>
-        <legend>Calculator Properties</legend>
-        <table>
-            <tr>
-                <td>Decimal Places</td>
-                <td><input id="builder_decimal_places" type="number" min="0" max="10" step="1"></td>
-            </tr>
-            <tr>
-                <td>Dimension</td>
-                <td>
-                    <select id="builder_dimension">
-                        <!-- Options will be populated dynamically -->
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Measure</td>
-                <td>
-                    <select id="builder_measure">
-                        <!-- Options will be populated dynamically -->
-                    </select>
-                </td>
-            </tr>
-        </table>
-        <input type="submit" style="display:none;">
-    </fieldset>
-</form>
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+        <style>
+     
+            .panel-content {
+                padding: 10px;
+            }
+            label {
+                display: block;
+                margin-bottom: 5px;
+            }
+        </style>
+        <div class="panel-content">
+            <label for="modelInput">Model:</label>
+            <input type="text" id="modelInput" />
+        </div>
+    `;
 
+    class BuilderPanel extends HTMLElement {
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({ mode: 'open' });
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
+            this._modelInput = this._shadowRoot.getElementById('modelInput');
+            this._modelInput.addEventListener('input', this._onModelInputChange.bind(this));
+        }
 
-  
-		<style>
-		:host {
-			display: block;
-			padding: 1em 1em 1em 1em;
-		}
-		</style>
-	`;
+        _onModelInputChange(event) {
+            // Dispatch a custom event or handle the model input change
+            let modelChangeEvent = new CustomEvent('modelChange', { detail: { model: this._modelInput.value } });
+            this.dispatchEvent(modelChangeEvent);
+        }
 
-	class CalculatorBuilderPanel extends HTMLElement {
-		constructor() {
-			super();
-			this._shadowRoot = this.attachShadow({mode: "open"});
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this._shadowRoot.getElementById("form").addEventListener("submit", this._submit.bind(this));
-		}
+        set model(value) {
+            this._modelInput.value = value;
+        }
 
-		_submit(e) {
-			e.preventDefault();
-			this.dispatchEvent(new CustomEvent("propertiesChanged", {
-					detail: {
-						properties: {
-		   decimalPlaces: this.decimalPlaces,
-                    dimension: this.dimension,
-                    measure: this.measure
-						}
-					}
-			}));
-		}
-
-		set decimalPlaces(newDecimalPlaces) {
-			this._shadowRoot.getElementById("builder_decimal_places").value = newDecimalPlaces;
-		}
-
-		get decimalPlaces() {
-			return this._shadowRoot.getElementById("builder_decimal_places").value;
-		}
-	
-	set dimension(newDimension) {
-        this._shadowRoot.getElementById("builder_dimension").value = newDimension;
+        get model() {
+            return this._modelInput.value;
+        }
     }
 
-    get dimension() {
-        return this._shadowRoot.getElementById("builder_dimension").value;
-    }
-
-    set measure(newMeasure) {
-        this._shadowRoot.getElementById("builder_measure").value = newMeasure;
-    }
-
-    get measure() {
-        return this._shadowRoot.getElementById("builder_measure").value;
-    }
-	}
-
-	customElements.define("com-sap-sample-calculator-builder", CalculatorBuilderPanel);
+    customElements.define('sac-cockpit-builder', BuilderPanel);
 })();
